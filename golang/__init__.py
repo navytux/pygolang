2506fa5f072e76e8badec4c779988d461e44c126
Kirill Smelkov <kirr@nexedi.com>
2019-01-14 14:25:02 +0300

    golang: Fix typo in __all__
    A comma was missing - this way last element was 'funcgimport', not
    'func' and 'gimport', and so `from golang import *` was failing.

--- golang/__init__.py
+++ golang/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2018  Nexedi SA and Contributors.
-#                     Kirill Smelkov <kirr@nexedi.com>
+# Copyright (C) 2018-2019  Nexedi SA and Contributors.
+#                          Kirill Smelkov <kirr@nexedi.com>
 #
 # This program is free software: you can Use, Study, Modify and Redistribute
 # it under the terms of the GNU General Public License version 3, or (at your
@@ -28,7 +28,7 @@
 ...
 """
 
-__all__ = ['method', 'go', 'chan', 'select', 'default', 'defer', 'panic', 'recover', 'func' 'gimport']
+__all__ = ['method', 'go', 'chan', 'select', 'default', 'defer', 'panic', 'recover', 'func', 'gimport']
 
 from golang._gopath import gimport  # make gimport available from golang
 import inspect, threading, collections, random, sys
