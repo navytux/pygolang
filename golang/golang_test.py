e918c6c9e3d3b87d60ff6200396d8bc74f5021c3
Kirill Smelkov <kirr@nexedi.com>
2019-07-30 18:02:02 +0300

    .

--- golang/golang_test.py
+++ golang/golang_test.py
@@ -20,7 +20,8 @@
 
 from __future__ import print_function, absolute_import
 
-from golang import go, chan, select, default, nilchan, _PanicError, func, panic, defer, recover
+from golang import go, chan, select, default, nilchan, _PanicError, func, \
+                panic, defer, recover, gimport
 from pytest import raises
 from os.path import dirname
 import os, sys, time, threading, inspect, subprocess
@@ -836,6 +837,48 @@ def bench_defer(b):
 
     for i in xrange(b.N):
         _()
+
+
+# ---- gimport ----
+
+# tgopath sets GOPATH to testdata/src during test execution.
+@pytest.fixture
+def tgopath():
+    gopath = os.environ.get('GOPATH')
+    os.environ['GOPATH'] = '%s/testdata' % (dirname(__file__),)
+    yield
+    if gopath is None:
+        del os.environ['GOPATH']
+    else:
+        os.environ['GOPATH'] = gopath
+
+
+def test_gimport_module(tgopath):
+    hello = gimport('lab.nexedi.com/kirr/hello')
+    assert hello.TAG == 'gopath: test: hello.py'
+    hello.TAG = 'loaded'
+
+    # verify second gimport does not reload
+    hello2 = gimport('lab.nexedi.com/kirr/hello')
+    assert hello2 is hello
+
+    # even though hello2 is hello - the module could be reloaded.
+    # check it is not the case via .TAG .
+    assert hello.TAG == 'loaded', 'module was reloaded'
+
+
+def test_gimport_package(tgopath):
+    world = gimport('lab.nexedi.com/kirr/world')
+    assert world.TAG == 'gopath: test: world/__init__.py'
+    world.TAG = 'loaded'
+
+    # verify second gimport does not reload
+    world2 = gimport('lab.nexedi.com/kirr/world')
+    assert world2 is world
+
+    # even though world2 is world - the module could be reloaded.
+    # check it is not the case via .TAG .
+    assert world.TAG =='loaded', 'module was reloaded'
 
 
 # ---- misc ----
