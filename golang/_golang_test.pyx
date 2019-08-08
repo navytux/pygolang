b8f04d72872346d4b3ddeb015a713ef94848fd14
Kirill Smelkov <kirr@nexedi.com>
2019-08-08 23:13:47 +0300

    .

--- golang/_golang_test.pyx
+++ golang/_golang_test.pyx
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # cython: language_level=2
 # distutils: language=c++
+#
 # Copyright (C) 2018-2019  Nexedi SA and Contributors.
 #                          Kirill Smelkov <kirr@nexedi.com>
 #
@@ -25,8 +26,8 @@ from __future__ import print_function, absolute_import
 from golang cimport go, chan, _chan, makechan, pychan, nil, select, _send,  \
     _recv, _recv_, _default, panic, pypanic, topyexc
 
-# small tests that verifie pyx-level channel API.
-# the work of channels themselves is mostly thoroughly excersized in golang_test.py
+# small tests that verifies pyx-level channel API.
+# the work of channels themselves is exercised thoroughly mostly in golang_test.py
 
 import time
 #from cpython cimport PY_MAJOR_VERSION
@@ -105,7 +106,7 @@ cdef void waitBlocked(_chan *ch, bint rx, bint tx) nogil:
             time.sleep(0)   # yield to another thread / coroutine
 
 
-# `with pypanicWhenBlocked` hooks into golang _blockforever to raise panic with
+# `with pypanicWhenBlocked` hooks into libgolang _blockforever to raise panic with
 # "t: blocks forever" instead of blocking.
 cdef class pypanicWhenBlocked:
     def __enter__(t):
@@ -120,6 +121,7 @@ cdef void _panicblocked() nogil:
     panic("t: blocks forever")
 
 
+# small test to verify pyx(nogil) channels.
 cdef extern from *:
     ctypedef bint cbool "bool"
 
@@ -165,6 +167,7 @@ def test_chan_nogil():
         _test_chan_nogil()
 
 
+# small test to verify pyx(nogil) go.
 cdef void _test_go_nogil() nogil except +topyexc:
     cdef chan[void] done = makechan[void]()
     cdef int i = 111
@@ -181,6 +184,51 @@ def test_go_nogil():
         _test_go_nogil()
 
 
+# verify that send/recv/select correctly route their onstack arguments through onheap proxies.
+cdef _test_chan_vs_stackdeadwhileparked() nogil except +topyexc:
+    # problem: under greenlet g's stack lives on system stack and is swapped as needed
+    # onto heap and back on g switch. This way if e.g. recv() is called with
+    # prx pointing to stack, and the stack is later copied to heap and replaced
+    # with stack of another g, the sender, if writing to original prx directly,
+    # will write to stack of different g, and original recv g, after wakeup,
+    # will see unchanged memory - with stack content that was saved to heap.
+    #
+    # to avoid this, send/recv/select create onheap proxies for onstack
+    # arguments and use those proxies as actual argument for send/receive.
+
+    # usestack_and_call pushes C-stack down and calls f from that.
+    # C-stack pushdown is used to make sure that when f will block and switched
+    # to another g, greenlet will save f's C-stack frame onto heap.
+    #
+    #   ---  ~~~
+    #             stack of another g
+    #   ---  ~~~
+    #
+    #    .
+    #    .
+    #    .
+    #
+    #    f    ->  heap
+    def usestack_and_call(f, nframes=128):
+        if nframes == 0:
+            return f()
+        return usestack_and_call(f, nframes-1)
+
+    # recv
+    ch = makechan[int]()
+    def _():
+        waitBlocked(ch._rawchan(), rx=True)
+        def _():
+            cdef int tx = 111
+            ch.send(&tx)
+        usestack_and_call(_)
+    go(_)
+    def _():
+        cdef int rx
+        ch.recv(&rx)
+        if rx != 111:
+            panic("recv(111) != 111")
+    usestack_and_call(_)
 
 
 
