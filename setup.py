fab282494a3825250ab9e3af5baae4dfc27adf9e
Kirill Smelkov <kirr@nexedi.com>
2019-09-03 20:08:14 +0300

    .

--- setup.py
+++ setup.py
@@ -202,9 +202,9 @@ setup(
                         include_dirs    = ['.', '3rdparty/include'],
                         define_macros   = [('BUILDING_LIBGOLANG', None)],
                         extra_compile_args = ['-std=gnu++11',
-                            #'-O0', '-fsanitize=thread'
+                            '-O0', '-fsanitize=thread'
                         ], # not c++11 as linux/list.h uses typeof
-                        #extra_link_args = ['-fsanitize=thread'],
+                        extra_link_args = ['-fsanitize=thread'],
                         soversion       = '0.1')],
     ext_modules = [
                     Ext('golang._golang',
--- test.sh
+++ test.sh
@@ -3,9 +3,8 @@
 python setup.py build_dso -i
 python setup.py build_ext -i
 
-#export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.5	# XXX debug with asan
-python -m pytest "$@"
-gpython -m pytest "$@"
+./trun python -m pytest "$@"
+./trun gpython -m pytest "$@"
 #gdb python -ex run -ex q --args python `which gpython` -m pytest "$@"
 
 #gpython g.py
