83f484381c8a2bf5f4a86c4b1e0e02e206aeab33
Kirill Smelkov <kirr@nexedi.com>
2019-09-04 14:16:13 +0300

    .

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
 #export CFLAGS="-fsanitize=thread"	# distutils take this for both C and C++
-export CFLAGS="-g -O0"	# distutils take this for both C and C++
+#export CFLAGS="-g -O0"	# distutils take this for both C and C++
 #export CFLAGS="-g -O0 -fsanitize=thread"	# distutils take this for both C and C++
+export CFLAGS="-g -fsanitize=thread"	# distutils take this for both C and C++
 
 #export CFLAGS="-O0 -g -fsanitize=thread"	# distutils take this for both C and C++
 #export CFLAGS="-O0 -g -fsanitize=address"	# distutils take this for both C and C++
@@ -13,9 +14,14 @@ python setup.py build_dso -i
 
 # XXX recheck
 # # FIXME why tsan does not detect problems if other extensions are also compiled with -race?
-# unset CFLAGS
+# XXX -> likely change in scheduling
+#unset CFLAGS
 
 python setup.py build_ext -i
+
+#export TSAN_OPTIONS="history_size=7 force_seq_cst_atomics=1 verbosity=1"
+#export TSAN_OPTIONS="history_size=7 verbosity=1"
+export TSAN_OPTIONS="history_size=7"
 
 #LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.5 python -m pytest "$@"
 ./trun python -m pytest "$@"
