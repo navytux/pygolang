f0b7f2dadd7bcd1cd4f96918dda377f77277d457
Kirill Smelkov <kirr@nexedi.com>
2019-09-06 17:05:55 +0300

    .

--- test.sh
+++ test.sh
@@ -1,30 +1,16 @@
 #!/bin/bash -xe
+# build/run pygolang tests in-tree while developing.
 
-#export CFLAGS="-fsanitize=thread"	# distutils take this for both C and C++
-#export CFLAGS="-g -O0"
+# distutils takes CFLAGS for both C and C++
+# distutils use CFLAGS also at link stage so no need to set LDFLAGS separately
 export CFLAGS="-g -O0 -fsanitize=thread"
-#export CFLAGS="-g -fsanitize=thread"
 #export CFLAGS="-g -O0 -fsanitize=address"
 
-#export CFLAGS="-O0 -g -fsanitize=thread"	# distutils take this for both C and C++
-#export CFLAGS="-O0 -g -fsanitize=address"	# distutils take this for both C and C++
-# distutils use CFLAGS also at link stage so no need to set LDFLAGS separately
-#export LDFLAGS="-kirr"
-
 python setup.py build_dso -i
-
-# XXX recheck
-# # FIXME why tsan does not detect problems if other extensions are also compiled with -race?
-# XXX -> likely change in scheduling
-#unset CFLAGS
-
 python setup.py build_ext -i
 
-#export TSAN_OPTIONS="history_size=7 force_seq_cst_atomics=1 verbosity=1"
-#export TSAN_OPTIONS="history_size=7 verbosity=1"
 #export TSAN_OPTIONS="history_size=7"
 
-#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.5 python -m pytest "$@"
 ./trun python -m pytest "$@"
 #./trun gpython -m pytest "$@"
 #gdb python -ex run -ex q --args python `which gpython` -m pytest "$@"
