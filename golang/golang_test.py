navytux/pygolang

commit 3b241983814e5836abae004d8549a68572731328

Port/move channels to C/C++/Pyx
- Move channels implementation to be done in C++ inside libgolang. The
  code and logic is based on previous Python-level channels
  implementation, but the new code is just C++ and does not depend on
  Python nor GIL at all, and so works without GIL if libgolang
  runtime works without GIL(*).

  (*) for example "thread" runtime works without GIL, while "gevent" runtime
      acquires GIL on every semaphore acquire.

  New channels implementation is located in δ(libgolang.cpp).

- Provide low-level C channels API to the implementation. The low-level
  C API was inspired by Libtask[1] and Plan9/Libthread[2].

  [1] Libtask: a Coroutine Library for C and Unix. https://swtch.com/libtask.
  [2] http://9p.io/magic/man2html/2/thread.

- Provide high-level C++ channels API that provides type-safety and
  automatic channel lifetime management.

  Overview of C and C++ APIs are in δ(libgolang.h).

- Expose C++ channels API at Pyx level as Cython/nogil API so that Cython
  programs could use channels with ease and without need to care about
  lifetime management and low-level details.

  Overview of Cython/nogil channels API is in δ(README.rst) and
  δ(_golang.pxd).

- Turn Python channels to be tiny wrapper around chan<PyObject>.

Implementation note:

- gevent case needs special care because greenlet, which gevent uses,
  swaps coroutine stack from C stack to heap on coroutine park, and
  replaces that space on C stack with stack of activated coroutine
  copied back from heap. This way if an object on g's stack is accessed
  while g is parked it would be memory of another g's stack.

  The channels implementation explicitly cares about this issue so that
  stack -> * channel send, or * -> stack channel receive work correctly.

  It should be noted that greenlet approach, which it inherits from
  stackless, is not only a bit tricky, but also comes with overhead
  (stack <-> heap copy), and prevents a coroutine to migrate from 1 OS

golang/golang_test.py
@@ -159,6 +159,11 @@ def test_chan():
     assert w2() is not None
     ch = None
     gc.collect()
+    # pypy needs another GC run: pychan does Py_DECREF on buffered objects, but
+    # on pypy cpyext objects are not deallocated from Py_DECREF even if
+    # ob_refcnt goes to zero - the deallocation is delayed until GC run.
+    # see also: http://doc.pypy.org/en/latest/discussion/rawrefcount.html
+    gc.collect()
     assert w1() is None
     assert w2() is None
 

golang/_golang.pxd
@@ -23,6 +23,7 @@ Cython/nogil API
 ----------------
 
 - `go` spawns lightweight thread.
+- `chan[T]`, `makechan[T]` and `select` provide C-level channels with Go semantic.
 - `panic` stops normal execution of current goroutine by throwing a C-level exception.
 
 Everything in Cython/nogil API do not depend on Python runtime and in
@@ -38,9 +39,15 @@ Golang.py runtime
 
 In addition to Cython/nogil API, golang.pyx provides runtime for golang.py:
 
+- Python-level channels are represented by pychan + pyselect.
 - Python-level panic is represented by pypanic.
 """
 
+
+from libcpp cimport nullptr_t, nullptr as nil
+from libcpp.utility cimport pair
+cdef extern from *:
+    ctypedef bint cbool "bool"
 
 # nogil pyx-level golang API.
 #
@@ -58,14 +65,63 @@ cdef extern from "golang/libgolang.h" namespace "golang" nogil:
 
     void go(...)    # typechecking is done by C
 
+    struct _chan
+    cppclass chan[T]:
+        chan();
+
+        # send/recv/close
+        void send(const T&)
+        T recv()
+        pair[T, cbool] recv_()
+        void close()
+
+        # send/recv in select
+        _selcase sends(const T *ptx)
+        _selcase recvs()
+        _selcase recvs(T* prx)
+        _selcase recvs(T* prx, cbool *pok)
+
+        # length/capacity
+        unsigned len()
+        unsigned cap()
+
+        # compare wrt nil; =nil
+        cbool operator==(nullptr_t)
+        cbool operator!=(nullptr_t)
+        void operator=(nullptr_t)
+
+        # for tests
+        _chan *_rawchan()
+
+    chan[T] makechan[T]()
+    chan[T] makechan[T](unsigned size)
+
+    struct structZ:
+        pass
+
+    enum _chanop:
+        _CHANSEND
+        _CHANRECV
+        _DEFAULT
+    struct _selcase:
+        _chanop op
+        void    *data
+        cbool   *rxok
+    const _selcase default "golang::_default"
+
+    int select(_selcase casev[])
+
 
 # ---- python bits ----
 
 cdef void topyexc() except *
 cpdef pypanic(arg)
 
+# pychan is chan<object>
+from cpython cimport PyObject
+ctypedef PyObject *pPyObject # https://github.com/cython/cython/issues/534
 from cython cimport final
 
 @final
 cdef class pychan:
-    cdef dict __dict__
+    cdef chan[pPyObject] ch

golang/_golang_test.pyx
@@ -26,21 +26,30 @@
 
 from __future__ import print_function, absolute_import
 
-from golang cimport go, pychan, panic, pypanic, topyexc
-from golang import nilchan
-from golang import _golang
+from golang cimport go, chan, _chan, makechan, pychan, nil, select, \
+    default, structZ, panic, pypanic, topyexc, cbool
 
-from golang import time
+cdef extern from "golang/libgolang.h" namespace "golang" nogil:
+    int _tchanrecvqlen(_chan *ch)
+    int _tchansendqlen(_chan *ch)
+    void (*_tblockforever)()
 
-# pylen_{recv,send}q returns len(pych._{recv,send}q)
+# pylen_{recv,send}q returns len(_chan._{recv,send}q)
 def pylen_recvq(pychan pych not None): # -> int
-    if pych is nilchan:
+    if pych.ch == nil:
         raise AssertionError('len(.recvq) on nil channel')
-    return len(pych._recvq)
+    return _tchanrecvqlen(pych.ch._rawchan())
 def pylen_sendq(pychan pych not None): # -> int
-    if pych is nilchan:
+    if pych.ch == nil:
         raise AssertionError('len(.sendq) on nil channel')
-    return len(pych._sendq)
+    return _tchansendqlen(pych.ch._rawchan())
+
+# runtime/libgolang_test.cpp
+cdef extern from *:
+    """
+    extern void waitBlocked(golang::_chan *ch, bool rx, bool tx);
+    """
+    void waitBlocked(_chan *, bint rx, bint tx) nogil except +topyexc
 
 # pywaitBlocked waits till a receive or send pychan operation blocks waiting on the channel.
 #
@@ -49,7 +58,8 @@ def pywaitBlocked(pychanop):
     if pychanop.__self__.__class__ is not pychan:
         pypanic("wait blocked: %r is method of a non-chan: %r" % (pychanop, pychanop.__self__.__class__))
     cdef pychan pych = pychanop.__self__
-    recv = send = False
+    cdef bint recv = False
+    cdef bint send = False
     if pychanop.__name__ == "recv":     # XXX better check PyCFunction directly
         recv = True
     elif pychanop.__name__ == "send":   # XXX better check PyCFunction directly
@@ -57,41 +67,84 @@ def pywaitBlocked(pychanop):
     else:
         pypanic("wait blocked: unexpected chan method: %r" % (pychanop,))
 
-    t0 = time.now()
-    while 1:
-        with pych._mu:
-            if recv and pylen_recvq(pych) > 0:
-                return
-            if send and pylen_sendq(pych) > 0:
-                return
-        now = time.now()
-        if now-t0 > 10: # waited > 10 seconds - likely deadlock
-            pypanic("deadlock")
-        time.sleep(0)   # yield to another thread / coroutine
+    with nogil:
+        waitBlocked(pych.ch._rawchan(), recv, send)
 
 
-# `with pypanicWhenBlocked` hooks into _golang._blockforever to raise panic with
+# `with pypanicWhenBlocked` hooks into libgolang _blockforever to raise panic with
 # "t: blocks forever" instead of blocking.
 cdef class pypanicWhenBlocked:
     def __enter__(t):
-        assert _golang._tblockforever is None
-        _golang._tblockforever = _panicblocked
+        global _tblockforever
+        _tblockforever = _panicblocked
         return t
 
     def __exit__(t, typ, val, tb):
-        _golang._tblockforever = None
+        _tblockforever = NULL
 
-def _panicblocked():
-    pypanic("t: blocks forever")
+cdef void _panicblocked() nogil:
+    panic("t: blocks forever")
+
+
+# small test to verify pyx(nogil) channels.
+ctypedef struct Point:
+    int x
+    int y
+
+# TODO kill this and teach Cython to coerce pair[X,Y] -> (X,Y)
+cdef (int, cbool) recv_(chan[int] ch) nogil:
+    _ = ch.recv_()
+    return (_.first, _.second)
+
+cdef void _test_chan_nogil() nogil except +topyexc:
+    cdef chan[structZ] done = makechan[structZ]()
+    cdef chan[int]     chi  = makechan[int](1)
+    cdef chan[Point]   chp  = makechan[Point]()
+    chp = nil   # reset to nil
+
+    cdef int i, j
+    cdef Point p
+    cdef cbool jok
+
+    i=+1; chi.send(i)
+    j=-1; j = chi.recv()
+    if not (j == i):
+        panic("send -> recv != I")
+
+    i = 2
+    _=select([
+        done.recvs(),           # 0
+        chi.sends(&i),          # 1
+        chp.recvs(&p),          # 2
+        chi.recvs(&j, &jok),    # 3
+        default,                # 4
+    ])
+    if _ != 1:
+        panic("select: selected !1")
+
+    j, jok = recv_(chi)
+    if not (j == 2 and jok == True):
+        panic("recv_ != (2, true)")
+
+    chi.close()
+    j, jok = recv_(chi)
+    if not (j == 0 and jok == False):
+        panic("recv_ from closed != (0, false)")
+
+def test_chan_nogil():
+    with nogil:
+        _test_chan_nogil()
 
 
 # small test to verify pyx(nogil) go.
 cdef void _test_go_nogil() nogil except +topyexc:
-    go(_work, 111)
-    # TODO wait till _work is done
-cdef void _work(int i) nogil:
+    cdef chan[structZ] done = makechan[structZ]()
+    go(_work, 111, done)
+    done.recv()
+cdef void _work(int i, chan[structZ] done) nogil:
     if i != 111:
         panic("_work: i != 111")
+    done.close()
 
 def test_go_nogil():
     with nogil:
@@ -101,9 +154,14 @@ def test_go_nogil():
 # runtime/libgolang_test_c.c
 cdef extern from * nogil:
     """
+    extern "C" void _test_chan_c();
     extern "C" void _test_go_c();
     """
+    void _test_chan_c() except +topyexc
     void _test_go_c()   except +topyexc
+def test_chan_c():
+    with nogil:
+        _test_chan_c()
 def test_go_c():
     with nogil:
         _test_go_c()
@@ -111,9 +169,24 @@ def test_go_c():
 # runtime/libgolang_test.cpp
 cdef extern from * nogil:
     """
+    extern void _test_chan_cpp_refcount();
+    extern void _test_chan_cpp();
+    extern void _test_chan_vs_stackdeadwhileparked();
     extern void _test_go_cpp();
     """
+    void _test_chan_cpp_refcount()              except +topyexc
+    void _test_chan_cpp()                       except +topyexc
+    void _test_chan_vs_stackdeadwhileparked()   except +topyexc
     void _test_go_cpp()                         except +topyexc
+def test_chan_cpp_refcount():
+    with nogil:
+        _test_chan_cpp_refcount()
+def test_chan_cpp():
+    with nogil:
+        _test_chan_cpp()
+def test_chan_vs_stackdeadwhileparked():
+    with nogil:
+        _test_chan_vs_stackdeadwhileparked()
 def test_go_cpp():
     with nogil:
         _test_go_cpp()

golang/libgolang.h
@@ -22,9 +22,10 @@
 
 // Library Libgolang provides Go-like features for C and C++.
 //
-// Library Libgolang provides goroutines and other
+// Library Libgolang provides goroutines, channels with Go semantic and other
 // accompanying features. The library consists of high-level type-safe C++ API,
-// and low-level unsafe C API.
+// and low-level unsafe C API. The low-level C API was inspired by Libtask[1]
+// and Plan9/Libthread[2].
 //
 // The primary motivation for Libgolang is to serve as runtime for golang.pyx -
 // - Cython part of Pygolang project. However Libgolang is independent of
@@ -35,12 +36,29 @@
 // C++-level API
 //
 //  - `go` spawns new task.
+//  - `chan<T>`, and `select` provide channels with Go semantic and automatic
+//    lifetime management.
 //  - `sleep` pauses current task.
 //  - `panic` throws exception that represent C-level panic.
 //
 // For example:
 //
-//      go(worker, 1);                  // spawn worker(int)
+//      chan<int> ch = makechan<int>(); // create new channel
+//      go(worker, ch, 1);              // spawn worker(chan<int>, int)
+//      ch.send(1)
+//      j = ch.recv()
+//
+//      _ = select({
+//          _default,       // 0
+//          ch.sends(&i),   // 1
+//          ch.recvs(&j),   // 2
+//      });
+//      if (_ == 0)
+//          // default case selected
+//      if (_ == 1)
+//          // case 1 selected: i sent to ch
+//      if (_ == 2)
+//          // case 2 selected: j received from ch
 //
 //      if (<bug condition>)
 //          panic("bug");
@@ -49,6 +67,10 @@
 // C-level API
 //
 //  - `_taskgo` spawns new task.
+//  - `_makechan` creates raw channel with Go semantic.
+//  - `_chanxincref` and `_chanxdecref` manage channel lifetime.
+//  - `_chansend` and `_chanrecv` send/receive over raw channel.
+//  - `_chanselect`, `_selsend`, `_selrecv`, ... provide raw select functionality.
 //  - `tasknanosleep` pauses current task.
 //
 //
@@ -65,6 +87,10 @@
 //
 // Once again, Libgolang itself is independent from Python, and other runtimes
 // are possible.
+//
+//
+// [1] Libtask: a Coroutine Library for C and Unix. https://swtch.com/libtask.
+// [2] http://9p.io/magic/man2html/2/thread.
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -104,10 +130,88 @@ LIBGOLANG_API void _taskgo(void (*f)(void *arg), void *arg);
 LIBGOLANG_API void _tasknanosleep(uint64_t dt);
 LIBGOLANG_API uint64_t _nanotime(void);
 
+typedef struct _chan _chan;
+LIBGOLANG_API _chan *_makechan(unsigned elemsize, unsigned size);
+LIBGOLANG_API void _chanxincref(_chan *ch);
+LIBGOLANG_API void _chanxdecref(_chan *ch);
+LIBGOLANG_API int  _chanrefcnt(_chan *ch);
+LIBGOLANG_API void _chansend(_chan *ch, const void *ptx);
+LIBGOLANG_API void _chanrecv(_chan *ch, void *prx);
+LIBGOLANG_API bool _chanrecv_(_chan *ch, void *prx);
+LIBGOLANG_API void _chanclose(_chan *ch);
+LIBGOLANG_API unsigned _chanlen(_chan *ch);
+LIBGOLANG_API unsigned _chancap(_chan *ch);
+
+enum _chanop {
+    _CHANSEND   = 0,
+    _CHANRECV   = 1,
+    _DEFAULT    = 2,
+};
+
+// _selcase represents one select case.
+typedef struct _selcase {
+    _chan           *ch;    // channel
+    enum _chanop    op;     // chansend/chanrecv/default
+    void            *data;  // chansend: ptx; chanrecv: prx
+    bool            *rxok;  // chanrecv: where to save ok if !NULL; otherwise not used
+} _selcase;
+
+LIBGOLANG_API int _chanselect(const _selcase *casev, int casec);
+
+// _selsend creates `_chansend(ch, ptx)` case for _chanselect.
+static inline
+_selcase _selsend(_chan *ch, const void *ptx) {
+    _selcase _ = {
+        .ch     = ch,
+        .op     = _CHANSEND,
+        .data   = (void *)ptx,
+        .rxok   = NULL,
+    };
+    return _;
+}
+
+// _selrecv creates `_chanrecv(ch, prx)` case for _chanselect.
+static inline
+_selcase _selrecv(_chan *ch, void *prx) {
+    _selcase _ = {
+        .ch     = ch,
+        .op     = _CHANRECV,
+        .data   = prx,
+        .rxok   = NULL,
+    };
+    return _;
+}
+
+// _selrecv_ creates `*pok = _chanrecv_(ch, prx)` case for _chanselect.
+static inline
+_selcase _selrecv_(_chan *ch, void *prx, bool *pok) {
+    _selcase _ = {
+        .ch     = ch,
+        .op     = _CHANRECV,
+        .data   = prx,
+        .rxok   = pok,
+    };
+    return _;
+}
+
+// _default represents default case for _chanselect.
+extern LIBGOLANG_API const _selcase _default;
+
 
 // libgolang runtime - the runtime must be initialized before any other libgolang use.
 typedef struct _libgolang_sema _libgolang_sema;
+typedef enum _libgolang_runtime_flags {
+    // STACK_DEAD_WHILE_PARKED indicates that it is not safe to access
+    // goroutine's stack memory while the goroutine is parked.
+    //
+    // for example gevent/greenlet/stackless use it because they copy g's stack
+    // to heap on park and back on unpark. This way if objects on g's stack
+    // were accessed while g was parked it would be memory of another g's stack.
+    STACK_DEAD_WHILE_PARKED = 1,
+} _libgolang_runtime_flags;
 typedef struct _libgolang_runtime_ops {
+    _libgolang_runtime_flags    flags;
+
     // go should spawn a task (coroutine/thread/...).
     void    (*go)(void (*f)(void *), void *arg);
 
@@ -136,6 +240,11 @@ typedef struct _libgolang_runtime_ops {
 LIBGOLANG_API void _libgolang_init(const _libgolang_runtime_ops *runtime_ops);
 
 
+// for testing
+LIBGOLANG_API int _tchanrecvqlen(_chan *ch);
+LIBGOLANG_API int _tchansendqlen(_chan *ch);
+LIBGOLANG_API extern void (*_tblockforever)(void);
+
 #ifdef __cplusplus
 }}
 #endif
@@ -145,8 +254,12 @@ LIBGOLANG_API void _libgolang_init(const _libgolang_runtime_ops *runtime_ops);
 
 #ifdef __cplusplus
 
+#include <exception>
 #include <functional>
+#include <initializer_list>
 #include <memory>
+#include <type_traits>
+#include <utility>
 
 namespace golang {
 
@@ -160,6 +273,122 @@ static inline void go(F /*std::function<void(Argv...)>*/ f, Argv... argv) {
         (*frun)();
         // frun deleted here on normal exit or panic.
     }, frun);
+}
+
+template<typename T> class chan;
+template<typename T> static chan<T> makechan(unsigned size=0);
+
+// chan<T> provides type-safe wrapper over _chan.
+//
+// chan<T> is automatically reference-counted and is safe to use from multiple
+// goroutines simultaneously.
+template<typename T>
+class chan {
+    _chan *_ch;
+
+public:
+    inline chan() { _ch = NULL; } // nil channel if not explicitly initialized
+    friend chan<T> makechan<T>(unsigned size);
+    inline ~chan() { _chanxdecref(_ch); _ch = NULL; }
+
+    // = nil
+    inline chan(nullptr_t) { _ch = NULL; }
+    inline chan& operator=(nullptr_t) { _chanxdecref(_ch); _ch = NULL; return *this; }
+    // copy
+    inline chan(const chan& from) { _ch = from._ch; _chanxincref(_ch); }
+    inline chan& operator=(const chan& from) {
+        if (this != &from) {
+            _chanxdecref(_ch); _ch = from._ch; _chanxincref(_ch);
+        }
+        return *this;
+    }
+    // move
+    inline chan(chan&& from) { _ch = from._ch; from._ch = NULL; }
+    inline chan& operator=(chan&& from) {
+        if (this != &from) {
+            _chanxdecref(_ch); _ch = from._ch; from._ch = NULL;
+        }
+        return *this;
+    }
+
+    // _chan does plain memcpy to copy elements.
+    // TODO allow all types (e.g. element=chan )
+    static_assert(std::is_trivially_copyable<T>::value, "TODO chan<T>: T copy is not trivial");
+
+    // send/recv/close
+    inline void send(const T &ptx)     { _chansend(_ch, &ptx);                  }
+    inline T recv()                    { T rx; _chanrecv(_ch, &rx); return rx;  }
+    inline std::pair<T,bool> recv_()   { T rx; bool ok = _chanrecv_(_ch, &rx);
+                                         return std::make_pair(rx, ok);         }
+    inline void close()                { _chanclose(_ch);                       }
+
+    // send/recv in select
+
+    // ch.sends creates `ch.send(*ptx)` case for select.
+    [[nodiscard]] inline _selcase sends(const T *ptx) { return _selsend(_ch, ptx); }
+
+    // ch.recvs creates `*prx = ch.recv()` case for select.
+    //
+    // if pok is provided the case is extended to `[*prx, *pok] = ch.recv_()`
+    // if both prx and pok are omitted the case is reduced to `ch.recv()`.
+    [[nodiscard]] inline _selcase recvs(T *prx=NULL, bool *pok=NULL) {
+        return _selrecv_(_ch, prx, pok);
+    }
+
+    // length/capacity
+    inline unsigned len()              { return _chanlen(_ch);         }
+    inline unsigned cap()              { return _chancap(_ch);         }
+
+    // compare wrt nil
+    inline bool operator==(nullptr_t)  { return (_ch == NULL); }
+    inline bool operator!=(nullptr_t)  { return (_ch != NULL); }
+
+    // compare wrt chan
+    inline bool operator==(const chan<T>& ch2)	{ return (_ch == ch2._ch); }
+    inline bool operator!=(const chan<T>& ch2)  { return (_ch != ch2._ch); }
+
+    // for testing
+    inline _chan *_rawchan()           { return _ch; }
+};
+
+// makechan<T> makes new chan<T> with capacity=size.
+template<typename T> static inline
+chan<T> makechan(unsigned size) {
+    chan<T> ch;
+    unsigned elemsize = std::is_empty<T>::value
+        ? 0          // eg struct{} for which sizeof() gives 1 - *not* 0
+        : sizeof(T);
+    ch._ch = _makechan(elemsize, size);
+    if (ch._ch == NULL)
+        throw std::bad_alloc();
+    return ch;
+}
+
+// structZ is struct{}.
+//
+// it's a workaround for e.g. makechan<struct{}> giving
+// "error: types may not be defined in template arguments".
+struct structZ{};
+
+// select, together with chan<T>.sends and chan<T>.recvs, provide type-safe
+// wrappers over _chanselect and _selsend/_selrecv/_selrecv_.
+//
+// Usage example:
+//
+//   _ = select({
+//       ch1.recvs(&v),         // 0
+//       ch2.recvs(&v, &ok),    // 1
+//       ch2.sends(&v),         // 2
+//       _default,              // 3
+//   })
+static inline                       // select({case1, case2, case3})
+int select(const std::initializer_list<const _selcase> &casev) {
+    return _chanselect(casev.begin(), casev.size());
+}
+
+template<size_t N> static inline    // select(casev_array)
+int select(const _selcase (&casev)[N]) {
+    return _chanselect(&casev[0], N);
 }
 
 

commit e9180de1e549f321d2cde81aa6e7bdb36dfbc5eb

golang: pyselect: Fix tx object reference leak on error exit
Before passing objects to _chanselect for send, pyselect increfs them, as just
send does, to indicate that one object reference is passed to channel buffer.
On exit, since only one case is actually executed by select, pyselect needs to
decref incref'ed object from not executed cases.

Pyselect already implements the latter cleanup, but currently the cleanup is
executed only if control flow reaches _chanselect at all. Which is a bug, since
pyselect can panic or raise an exception just in the middle of preparation phase.

-> Fix it by associating the finally-decref cleanup with whole
prepare+_chanselect code.

Without the fix, the second part of added test (abnormal exit) fails e.g. like:

        @mark.skipif(not hasattr(sys, 'getrefcount'),   # skipped e.g. on PyPy
                     reason="needs sys.getrefcount")
        def test_select_refleak():
            ch1 = chan()
            ch2 = chan()
            obj1 = object()
            obj2 = object()
            tx1 = (ch1.send, obj1)
            tx2 = (ch2.send, obj2)

            # normal exit
            gc.collect()
            nref1 = sys.getrefcount(obj1)
            nref2 = sys.getrefcount(obj2)
            _, _rx = select(
                tx1,        # 0
                tx2,        # 1
                default,    # 2
            )
            assert (_, _rx) == (2, None)
            gc.collect()
            assert sys.getrefcount(obj1) == nref1
            gc.collect()
            assert sys.getrefcount(obj1) == nref2

            # abnormal exit
            with raises(AttributeError) as exc:
                select(
                    tx1,        # 0
                    tx2,        # 1
                    'zzz',      # 2 causes pyselect to panic
                )
            assert exc.value.args == ("'str' object has no attribute '__self__'",)
            gc.collect()
    >       assert sys.getrefcount(obj1) == nref1
    E       assert 4 == 3
    E         -4
    E         +3

    golang/golang_test.py:690: AssertionError

The bug was introduced in 3b241983

golang/_golang.pyx
@@ -272,60 +272,61 @@ pydefault  = object()
 #       ...
 def pyselect(*pycasev):
     cdef int i, n = len(pycasev), selected
-    cdef vector[_selcase] casev = vector[_selcase](n)
+    cdef vector[_selcase] casev = vector[_selcase](n, default)
     cdef pychan pych
     cdef PyObject *_rx = NULL # all select recvs are setup to receive into _rx
     cdef cbool rxok = False   # (its ok as only one receive will be actually executed)
 
-    # prepare casev for chanselect
-    for i in range(n):
-        pycase = pycasev[i]
-        # default
-        if pycase is pydefault:
-            casev[i] = default
-
-        # send
-        elif type(pycase) is tuple:
-            if len(pycase) != 2:
-                pypanic("pyselect: invalid [%d]() case" % len(pycase))
-            _tcase = <PyTupleObject *>pycase
-
-            pysend = <object>(_tcase.ob_item[0])
-            if pysend.__self__.__class__ is not pychan:
-                pypanic("pyselect: send on non-chan: %r" % (pysend.__self__.__class__,))
-            pych = pysend.__self__
-
-            if pysend.__name__ != "send":       # XXX better check PyCFunction directly
-                pypanic("pyselect: send expected: %r" % (pysend,))
-
-            # wire ptx through pycase[1]
-            p_tx = &(_tcase.ob_item[1])
-            tx   = <object>(p_tx[0])
-
-            # incref tx as if corresponding channel is holding pointer to the object while it is being sent.
-            # we'll decref the object if it won't be sent.
-            # see pychan.send for details.
-            Py_INCREF(tx)
-            casev[i] = _selsend(pych._ch, p_tx)
-
-        # recv
-        else:
-            pyrecv = pycase
-            if pyrecv.__self__.__class__ is not pychan:
-                pypanic("pyselect: recv on non-chan: %r" % (pyrecv.__self__.__class__,))
-            pych = pyrecv.__self__
-
-            if pyrecv.__name__ == "recv":       # XXX better check PyCFunction directly
-                casev[i] = _selrecv(pych._ch, &_rx)
-            elif pyrecv.__name__ == "recv_":    # XXX better check PyCFunction directly
-                casev[i] = _selrecv_(pych._ch, &_rx, &rxok)
-            else:
-                pypanic("pyselect: recv expected: %r" % (pyrecv,))
-
     selected = -1
     try:
+        # prepare casev for chanselect
+        for i in range(n):
+            pycase = pycasev[i]
+            # default
+            if pycase is pydefault:
+                casev[i] = default
+
+            # send
+            elif type(pycase) is tuple:
+                if len(pycase) != 2:
+                    pypanic("pyselect: invalid [%d]() case" % len(pycase))
+                _tcase = <PyTupleObject *>pycase
+
+                pysend = <object>(_tcase.ob_item[0])
+                if pysend.__self__.__class__ is not pychan:
+                    pypanic("pyselect: send on non-chan: %r" % (pysend.__self__.__class__,))
+                pych = pysend.__self__
+
+                if pysend.__name__ != "send":       # XXX better check PyCFunction directly
+                    pypanic("pyselect: send expected: %r" % (pysend,))
+
+                # wire ptx through pycase[1]
+                p_tx = &(_tcase.ob_item[1])
+                tx   = <object>(p_tx[0])
+
+                # incref tx as if corresponding channel is holding pointer to the object while it is being sent.
+                # we'll decref the object if it won't be sent.
+                # see pychan.send for details.
+                Py_INCREF(tx)
+                casev[i] = _selsend(pych._ch, p_tx)
+
+            # recv
+            else:
+                pyrecv = pycase
+                if pyrecv.__self__.__class__ is not pychan:
+                    pypanic("pyselect: recv on non-chan: %r" % (pyrecv.__self__.__class__,))
+                pych = pyrecv.__self__
+
+                if pyrecv.__name__ == "recv":       # XXX better check PyCFunction directly
+                    casev[i] = _selrecv(pych._ch, &_rx)
+                elif pyrecv.__name__ == "recv_":    # XXX better check PyCFunction directly
+                    casev[i] = _selrecv_(pych._ch, &_rx, &rxok)
+                else:
+                    pypanic("pyselect: recv expected: %r" % (pyrecv,))
+
         with nogil:
             selected = _chanselect_pyexc(&casev[0], casev.size())
+
     finally:
         # decref not sent tx (see ^^^ send prepare)
         for i in range(n):

golang/golang_test.py
@@ -22,7 +22,7 @@ from __future__ import print_function, absolute_import
 
 from golang import go, chan, select, default, nilchan, _PanicError, func, panic, defer, recover
 from golang import sync
-from pytest import raises
+from pytest import raises, mark
 from os.path import dirname
 import os, sys, inspect, importlib
 from subprocess import Popen, PIPE
@@ -650,6 +650,46 @@ def test_select():
     done.recv()
     assert len_sendq(ch1) == len_recvq(ch1) == 0
     assert len_sendq(ch2) == len_recvq(ch2) == 0
+
+
+# verify that select does not leak references to passed objects.
+@mark.skipif(not hasattr(sys, 'getrefcount'),   # skipped e.g. on PyPy
+             reason="needs sys.getrefcount")
+def test_select_refleak():
+    ch1 = chan()
+    ch2 = chan()
+    obj1 = object()
+    obj2 = object()
+    tx1 = (ch1.send, obj1)
+    tx2 = (ch2.send, obj2)
+
+    # normal exit
+    gc.collect()
+    nref1 = sys.getrefcount(obj1)
+    nref2 = sys.getrefcount(obj2)
+    _, _rx = select(
+        tx1,        # 0
+        tx2,        # 1
+        default,    # 2
+    )
+    assert (_, _rx) == (2, None)
+    gc.collect()
+    assert sys.getrefcount(obj1) == nref1
+    gc.collect()
+    assert sys.getrefcount(obj1) == nref2
+
+    # abnormal exit
+    with raises(AttributeError) as exc:
+        select(
+            tx1,        # 0
+            tx2,        # 1
+            'zzz',      # 2 causes pyselect to panic
+        )
+    assert exc.value.args == ("'str' object has no attribute '__self__'",)
+    gc.collect()
+    assert sys.getrefcount(obj1) == nref1
+    gc.collect()
+    assert sys.getrefcount(obj1) == nref2
 
 
 # benchmark sync chan send vs recv on select side.