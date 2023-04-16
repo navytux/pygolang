7fb630c19e6981309982640472323ec5437e08eb
Kirill Smelkov <kirr@nexedi.com>
2023-04-16 18:47:23 +0300

    tox += CPython 3.10, 3.11
    Tests now pass with those versions.
    Remove py38-dbg from test matrix ad this version is now a bit outdated
    and having just py38 without dbg there seems enough.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # pygolang | pythonic package setup
-# Copyright (C) 2018-2022  Nexedi SA and Contributors.
+# Copyright (C) 2018-2023  Nexedi SA and Contributors.
 #                          Kirill Smelkov <kirr@nexedi.com>
 #
 # This program is free software: you can Use, Study, Modify and Redistribute
@@ -345,6 +345,8 @@ setup(
         Programming Language :: Python :: 3.7
         Programming Language :: Python :: 3.8
         Programming Language :: Python :: 3.9
+        Programming Language :: Python :: 3.10
+        Programming Language :: Python :: 3.11
         Programming Language :: Python :: Implementation :: CPython
         Programming Language :: Python :: Implementation :: PyPy
         Topic :: Software Development :: Interpreters
