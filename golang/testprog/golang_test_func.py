87f08cac6cbb4e9fe33df9500e307060b6a754d5
Kirill Smelkov <kirr@nexedi.com>
2025-08-13 12:15:39 +0300

    golang: tests: Push @func test one more level deep
    Previously it was testing @func inside another function, but now it is
    testing @func inside another function, which itself runs inside another
    function.
    
    This is preparatory change to verify @func vs closure variables in the
    next patches. So far everything works ok.
    
    /reviewed-by @jerome
    /reviewed-on https://lab.nexedi.com/nexedi/pygolang/-/merge_requests/37

--- golang/testprog/golang_test_func.py
+++ golang/testprog/golang_test_func.py
@@ -31,119 +31,121 @@ import gc
 
 
 if 1:       #def tmain():               +funcfunc
-    # test how @func(cls) works
-    # this also implicitly tests just @func, since @func(cls) uses that.
+    if 1:   #    def _tmain():          +funcfunc
+        # test how @func(cls) works
+        # this also implicitly tests just @func, since @func(cls) uses that.
 
-    class MyClass(object):
-        def __init__(self, v):
+        class MyClass(object):
+            def __init__(self, v):
+                self.v = v
+
+        zzz = zzz_orig = 'z'    # `@func(MyClass) def zzz` must not override zzz
+        @func(MyClass)
+        def zzz(self, v, x=2, **kkkkwww):
+            assert self.v == v
+            return v + 1
+        assert zzz is zzz_orig
+        assert zzz == 'z'
+
+        mstatic = mstatic_orig = 'mstatic'
+        @func(MyClass)
+        @staticmethod
+        def mstatic(v):
+            assert v == 5
+            return v + 1
+        assert mstatic is mstatic_orig
+        assert mstatic == 'mstatic'
+
+        mcls = mcls_orig = 'mcls'
+        @func(MyClass)
+        @classmethod
+        def mcls(cls, v):
+            assert cls is MyClass
+            assert v == 7
+            return v + 1
+        assert mcls is mcls_orig
+        assert mcls == 'mcls'
+
+        # undefined var after `@func(cls) def var` should be not set
+        assert 'var' not in locals()
+        @func(MyClass)
+        def var(self, v):
+            assert v == 8
+            return v + 1
+        gc.collect()    # pypy needs this to trigger _DelAttrAfterMeth GC
+        assert 'var' not in locals()
+
+
+        vproperty = vproperty_orig = 'vproperty'
+        @func(MyClass)
+        @property
+        def vproperty(self):
+            """documentation for vproperty"""
+            assert isinstance(self, MyClass)
+            return 'v%s' % self.v
+        assert vproperty is vproperty_orig
+        assert vproperty == 'vproperty'
+
+        @func(MyClass)
+        @MyClass.vproperty.setter
+        def _(self, v):
+            assert isinstance(self, MyClass)
             self.v = v
+        assert vproperty is vproperty_orig
+        assert vproperty == 'vproperty'
 
-    zzz = zzz_orig = 'z'    # `@func(MyClass) def zzz` must not override zzz
-    @func(MyClass)
-    def zzz(self, v, x=2, **kkkkwww):
-        assert self.v == v
-        return v + 1
-    assert zzz is zzz_orig
-    assert zzz == 'z'
-
-    mstatic = mstatic_orig = 'mstatic'
-    @func(MyClass)
-    @staticmethod
-    def mstatic(v):
-        assert v == 5
-        return v + 1
-    assert mstatic is mstatic_orig
-    assert mstatic == 'mstatic'
-
-    mcls = mcls_orig = 'mcls'
-    @func(MyClass)
-    @classmethod
-    def mcls(cls, v):
-        assert cls is MyClass
-        assert v == 7
-        return v + 1
-    assert mcls is mcls_orig
-    assert mcls == 'mcls'
-
-    # undefined var after `@func(cls) def var` should be not set
-    assert 'var' not in locals()
-    @func(MyClass)
-    def var(self, v):
-        assert v == 8
-        return v + 1
-    gc.collect()    # pypy needs this to trigger _DelAttrAfterMeth GC
-    assert 'var' not in locals()
+        @func(MyClass)
+        @MyClass.vproperty.deleter
+        def _(self):
+            assert isinstance(self, MyClass)
+            self.v = 'deleted'
+        assert vproperty is vproperty_orig
+        assert vproperty == 'vproperty'
 
 
-    vproperty = vproperty_orig = 'vproperty'
-    @func(MyClass)
-    @property
-    def vproperty(self):
-        """documentation for vproperty"""
-        assert isinstance(self, MyClass)
-        return 'v%s' % self.v
-    assert vproperty is vproperty_orig
-    assert vproperty == 'vproperty'
+        obj = MyClass(4)
+        assert obj.zzz(4)       == 4 + 1
+        assert obj.mstatic(5)   == 5 + 1
+        assert obj.mcls(7)      == 7 + 1
+        assert obj.var(8)       == 8 + 1
+        assert obj.v            == 4        # set by .zzz
+        assert obj.vproperty    == 'v4'
+        obj.vproperty = 5
+        assert obj.v            == 5
+        assert obj.vproperty    == 'v5'
+        del obj.vproperty
+        assert obj.v            == 'deleted'
+        assert obj.vproperty    == 'vdeleted'
+        assert MyClass.vproperty.__doc__ == "documentation for vproperty"""
 
-    @func(MyClass)
-    @MyClass.vproperty.setter
-    def _(self, v):
-        assert isinstance(self, MyClass)
-        self.v = v
-    assert vproperty is vproperty_orig
-    assert vproperty == 'vproperty'
+        # this tests that @func (used by @func(cls)) preserves decorated function signature
+        assert fmtargspec(MyClass.zzz) == '(self, v, x=2, **kkkkwww)'
 
-    @func(MyClass)
-    @MyClass.vproperty.deleter
-    def _(self):
-        assert isinstance(self, MyClass)
-        self.v = 'deleted'
-    assert vproperty is vproperty_orig
-    assert vproperty == 'vproperty'
+        assert MyClass.zzz.__module__       == __name__
+        assert MyClass.zzz.__name__         == 'zzz'
 
+        assert MyClass.mstatic.__module__   == __name__
+        assert MyClass.mstatic.__name__     == 'mstatic'
 
-    obj = MyClass(4)
-    assert obj.zzz(4)       == 4 + 1
-    assert obj.mstatic(5)   == 5 + 1
-    assert obj.mcls(7)      == 7 + 1
-    assert obj.var(8)       == 8 + 1
-    assert obj.v            == 4        # set by .zzz
-    assert obj.vproperty    == 'v4'
-    obj.vproperty = 5
-    assert obj.v            == 5
-    assert obj.vproperty    == 'v5'
-    del obj.vproperty
-    assert obj.v            == 'deleted'
-    assert obj.vproperty    == 'vdeleted'
-    assert MyClass.vproperty.__doc__ == "documentation for vproperty"""
+        assert MyClass.mcls.__module__      == __name__
+        assert MyClass.mcls.__name__        == 'mcls'
 
-    # this tests that @func (used by @func(cls)) preserves decorated function signature
-    assert fmtargspec(MyClass.zzz) == '(self, v, x=2, **kkkkwww)'
+        assert MyClass.var.__module__       == __name__
+        assert MyClass.var.__name__         == 'var'
 
-    assert MyClass.zzz.__module__       == __name__
-    assert MyClass.zzz.__name__         == 'zzz'
+        assert MyClass.vproperty.fget.__module__    == __name__
+        assert MyClass.vproperty.fset.__module__    == __name__
+        assert MyClass.vproperty.fdel.__module__    == __name__
+        assert MyClass.vproperty.fget.__name__      == 'vproperty'
+        assert MyClass.vproperty.fset.__name__      == '_'
+        assert MyClass.vproperty.fdel.__name__      == '_'
 
-    assert MyClass.mstatic.__module__   == __name__
-    assert MyClass.mstatic.__name__     == 'mstatic'
+        # test that func·func = func  (double _func calls are done internally for
+        # getter when handling @func(@MyClass.vproperty.setter)
+        def f(): pass
+        g = func(f)
+        h = func(g)
+        assert h is g
 
-    assert MyClass.mcls.__module__      == __name__
-    assert MyClass.mcls.__name__        == 'mcls'
-
-    assert MyClass.var.__module__       == __name__
-    assert MyClass.var.__name__         == 'var'
-
-    assert MyClass.vproperty.fget.__module__    == __name__
-    assert MyClass.vproperty.fset.__module__    == __name__
-    assert MyClass.vproperty.fdel.__module__    == __name__
-    assert MyClass.vproperty.fget.__name__      == 'vproperty'
-    assert MyClass.vproperty.fset.__name__      == '_'
-    assert MyClass.vproperty.fdel.__name__      == '_'
-
-    # test that func·func = func  (double _func calls are done internally for
-    # getter when handling @func(@MyClass.vproperty.setter)
-    def f(): pass
-    g = func(f)
-    h = func(g)
-    assert h is g
-
+#    _tmain()                           +funcfunc
 #tmain()                                +funcfunc
